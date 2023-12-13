31d3b1b2ce7910c64184bc197408e7f4b0485f3c
DimRev <dimarev444@gmail.com>
2023-12-13 21:35:29 +0200

    Edit : NoteVideo is able to embed Videos

--- apps/note/cmps/NoteVideo.jsx
+++ apps/note/cmps/NoteVideo.jsx
@@ -1,4 +1,9 @@
 export function NoteVideo({ note, deleteNote, editNote, from }) {
+  function getYouTubeVideoId(url) {
+    const match = url.match(/[?&]v=([^?&]+)/)
+    return match ? match[1] : null
+  }
+
   switch (from) {
     case 'noteList':
       return (
@@ -17,9 +22,17 @@ export function NoteVideo({ note, deleteNote, editNote, from }) {
             edit
           </button>
 
-          <h2>{note.info.title}</h2>
+          <iframe
+            src={`https://www.youtube.com/embed/${getYouTubeVideoId(
+              note.info.youtubeUrl
+            )}`}
+            title={note.info.title}
+            allowFullScreen></iframe>
         </article>
       )
     case 'noteEdit':
+
+    default:
+      return null
   }
 }
