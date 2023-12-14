3f15484832f7db4844d3ee17faac4b111ba12d4c
DimRev <dimarev444@gmail.com>
2023-12-14 07:07:20 +0200

    Style : Note list section

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -2,8 +2,8 @@ import { NotePreview } from "./NotePreview.jsx";
 
 export function NoteList({ notes , deleteNote, editNote, todoToggle}) {
   return (
-    <section className="note-list">
-      <ul>
+    <section className="note-list-section">
+      <ul className="note-list">
         {notes.map((note) => (
           <li key={note.id}><NotePreview note={note} deleteNote={deleteNote} editNote={editNote} todoToggle={todoToggle} from="noteList"/></li>
         ))}
