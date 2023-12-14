b92e17e1f93ceb4a3aa2711b5eaecdcb17d944c2
DimRev <dimarev444@gmail.com>
2023-12-15 01:07:46 +0200

    Add : Added logo

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,30 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation } = ReactRouterDOM
 
 export function AppHeader() {
+  const location = useLocation()
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>AppSus</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+  const getImageSrc = () => {
+    switch (location.pathname) {
+      case '/mail':
+        return 'assets/img/logos/SusMail.png'
+      case '/note':
+        return 'assets/img/logos/SusNote.png'
+      default:
+        return 'assets/img/logos/SusApp.png'
+    }
+  }
+
+  return (
+    <header className="app-header">
+      <Link to="/">
+        <img src={getImageSrc()} alt="" />
+      </Link>Lin
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/mail">Mail</NavLink>
+        <NavLink to="/note">Note</NavLink>
+      </nav>
     </header>
+  )
 }
