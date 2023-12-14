61ae9081143e1d0bf75e112000c0146e58a3c761
DimRev <dimarev444@gmail.com>
2023-12-14 07:23:58 +0200

    Edit : Add note type active

--- apps/note/cmps/NoteAdd.jsx
+++ apps/note/cmps/NoteAdd.jsx
@@ -3,34 +3,45 @@ import { NoteAddType } from './NoteAddType.jsx'
 
 export function NoteAdd({ addNote }) {
   const [type, setType] = useState('noteTxt')
+  const [selected, setSelected] = useState([true, false, false, false])
 
-  function onTypeChange(type) {
+  function onTypeChange(type, idx) {
     setType(type)
+    let isActives = [false, false, false, false]
+    isActives[idx] = true
+    setSelected(isActives)
+  }
+  function isActiveClass(isActive) {
+    return isActive ? 'active' : ''
   }
 
   return (
     <article className="note-add">
       <button
+        className={`type-btn ${isActiveClass(selected[0])}`}
         onClick={() => {
-          onTypeChange('noteTxt')
+          onTypeChange('noteTxt', 0)
         }}>
         Text
       </button>
       <button
+        className={`type-btn ${isActiveClass(selected[1])}`}
         onClick={() => {
-          onTypeChange('noteTodos')
+          onTypeChange('noteTodos', 1)
         }}>
         Todos
       </button>
       <button
+        className={`type-btn ${isActiveClass(selected[2])}`}
         onClick={() => {
-          onTypeChange('noteImg')
+          onTypeChange('noteImg', 2)
         }}>
         Img
       </button>
       <button
+        className={`type-btn ${isActiveClass(selected[3])}`}
         onClick={() => {
-          onTypeChange('noteVideo')
+          onTypeChange('noteVideo', 3)
         }}>
         Video
       </button>
