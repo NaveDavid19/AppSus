ca2ffb11cd540047bce9eac8fd5c22513fcf6d5d
DimRev <dimarev444@gmail.com>
2023-12-14 13:16:09 +0200

    Style : Ongoing styling

--- apps/note/cmps/ColorButtonsAdd.jsx
+++ apps/note/cmps/ColorButtonsAdd.jsx
@@ -1,53 +1,70 @@
-export function ColorButtonsAdd({
-  changeBackgroundColor
-}) {
-  return <details>
-        <summary>Colors</summary>
-        <button onClick={() => {
-      changeBackgroundColor('#faafa8');
-    }}>
-          coral
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#f39f76');
-    }}>
-          peach
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#fff8b8');
-    }}>
-          sand
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#e2f6d3');
-    }}>
-          mint
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#b4ddd3');
-    }}>
-          sage
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#aeccdc');
-    }}>
-          storm
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#f6e2dd');
-    }}>
-          blossom
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#e9e3d4');
-    }}>
-          clay
-        </button>
-        <button onClick={() => {
-      changeBackgroundColor('#efeff1');
-    }}>
-          chalk
-        </button>
-      </details>;
+const { useRef, useEffect } = React
+
+export function ColorButtonsAdd({ changeBackgroundColor }) {
+  const detailsRef = useRef()
+
+  const handleClick = (color) => {
+    changeBackgroundColor(color)
+  }
+
+  const handleButtonClick = (event, color) => {
+    event.stopPropagation()
+    handleClick(color)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (detailsRef.current && !detailsRef.current.contains(event.target)) {
+        detailsRef.current.removeAttribute('open')
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
+  return (
+    <details className="color-details" ref={detailsRef}>
+      <summary><i class="fa-solid fa-brush"></i></summary>
+      <button
+        type="button"
+        style={{ backgroundColor: '#faafa8' }}
+        onClick={(e) => handleButtonClick(e, '#faafa8')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#f39f76' }}
+        onClick={(e) => handleButtonClick(e, '#f39f76')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#fff8b8' }}
+        onClick={(e) => handleButtonClick(e, '#fff8b8')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#e2f6d3' }}
+        onClick={(e) => handleButtonClick(e, '#e2f6d3')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#b4ddd3' }}
+        onClick={(e) => handleButtonClick(e, '#b4ddd3')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#aeccdc' }}
+        onClick={(e) => handleButtonClick(e, '#aeccdc')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#f6e2dd' }}
+        onClick={(e) => handleButtonClick(e, '#f6e2dd')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#e9e3d4' }}
+        onClick={(e) => handleButtonClick(e, '#e9e3d4')}></button>
+      <button
+        type="button"
+        style={{ backgroundColor: '#efeff1' }}
+        onClick={(e) => handleButtonClick(e, '#efeff1')}></button>
+    </details>
+  )
 }
-  
