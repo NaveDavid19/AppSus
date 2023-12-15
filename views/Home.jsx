64448d17ab24a5173b00c26fa311206316acbfde
DimRev <dimarev444@gmail.com>
2023-12-16 00:01:13 +0200

    Edit : Added animations to navbuttons

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,22 +1,45 @@
-const { NavLink } = ReactRouterDOM
+const { useRef } = React
+const { useNavigate } = ReactRouterDOM
+import { utilService } from '../services/util.service.js'
 
 export function Home() {
+  const mailRef = useRef()
+  const noteRef = useRef()
+  const navigate = useNavigate()
+
+  function onAnimate(address) {
+    if (address === 'mail') {
+      utilService
+        .animateCSS(mailRef.current, 'backOutLeft')
+        .then(() => navigate('/mail'))
+    }
+    if (address === 'note') {
+      utilService
+        .animateCSS(noteRef.current, 'backOutRight')
+        .then(() => navigate('/note'))
+    }
+  }
+
   return (
     <div className="home-container">
       <h1>Welcome to AppSus!</h1>
       <p>Explore our apps:</p>
       <div className="app-links">
-        <NavLink to="/mail">
-          <img
-            src="assets/img/logos/susMail.png"
-            width="50"
-            height="50"
-            alt="Email App"
-          />
-        </NavLink>
-        <NavLink to="/note">
-          <img src="assets/img/logos/susNote.png" alt="Notes App" />
-        </NavLink>
+        <img
+          ref={mailRef}
+          onClick={() => onAnimate('mail')}
+          onMouseOver={() => utilService.animateCSS(mailRef.current, 'bounce')}
+          src="assets/img/logos/susMail.png"
+          alt="Email App"
+        />
+
+        <img
+          ref={noteRef}
+          onClick={() => onAnimate('note')}
+          onMouseOver={() => utilService.animateCSS(noteRef.current, 'bounce')}
+          src="assets/img/logos/susNote.png"
+          alt="Notes App"
+        />
       </div>
       <p>Discover the power of simplicity.</p>
     </div>
